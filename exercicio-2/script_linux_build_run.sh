8cbcf351c33902d8e1172bf9695f873adff06339
MatheusAlencarSlytz123 <103943332+MatheusAlencarSlytz123@users.noreply.github.com>
2024-07-14 12:02:48 -0300

    ajustado script build & run linux/unix

--- exercicio-2/script_linux_build_run.sh
+++ exercicio-2/script_linux_build_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -Wall src/funcionario.c tests/unity/unity.c -o exercicio-2-run
+gcc -Wall src/funcionario.c tests/teste.c tests/unity/unity.c -o exercicio-2-run
 read
 clear
 ./exercicio-2-run
